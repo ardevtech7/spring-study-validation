3d9964c66788688599a18d03b1f15e5e867f9f72
ardevtech7 <ardevtech7@gmail.com>
2023-01-14 01:25:02 +0900

    Spring Bean Validation V3

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,9 +1,28 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
+
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구사항으로 추가
+//    private Long id;
+//
+//    @NotBlank(groups = {SavaCheck.class, UpdateCheck.class})
+//    private String itemName;
+//
+//    @NotNull(groups = {SavaCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SavaCheck.class, UpdateCheck.class})
+//    private Integer price;
+//
+//    @NotNull(groups = {SavaCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = UpdateCheck.class) // 수정 요구사항 추가
+//    private Integer quantity;
 
     private Long id;
     private String itemName;
